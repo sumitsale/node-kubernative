ef66b192868f87a4de4ae6ac4478c2631a5df925
Sumit Sale <sumitsale@G3-MBP-1178.local>
2025-01-27 16:29:10 +0530

    First Commit

---
+++ controllers/PingController.js
@@ -0,0 +1,10 @@
+// controllers/PingController.js
+const express = require('express');
+const router = express.Router();
+
+// Ping route
+router.get('/ping', (req, res) => {
+    res.status(200).json({ message: 'Pong', timestamp: new Date() });
+});
+
+module.exports = router;
